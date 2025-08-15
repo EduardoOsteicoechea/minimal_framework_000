870c007121f548f8d3a0a4b78f77e8bdc597e992
eduar <eduar@EDUARDO>
2025-08-15 10:24:47 -0400

    update

--- check_db/create_article/index.php
+++ check_db/create_article/index.php
@@ -1,11 +1,18 @@
 <?php
 
-include '../_/db/actions/create_article_001.php';
-create_article_001("test_article", '
-
+try
 {
-  "title":"test article",
-  "content":"test content"
-}
 
-'); 
+  include '../_/db/actions/create_article_001.php';
+  create_article_001("test_article", '
+  
+  {
+    "title":"test article",
+    "content":"test content"
+    }
+    
+    '); 
+    
+  } catch (Exception $e) {
+    echo $e->getMessage();
+}
