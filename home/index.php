c4e539fce0d608ed79aedf744d049a56aca90ed8
eduar <eduar@EDUARDO>
2025-08-15 08:51:06 -0400

    update

--- home/index.php
+++ home/index.php
@@ -1,40 +1,50 @@
 <?php
 
-$databaseFile = '../my_database.sqlite';
+echo '
+ 
 
-try {
-  $pdo = new PDO("sqlite:$databaseFile");
 
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-  $sqlSelectData = "SELECT id, name, email FROM users";
 
-  $stmt = $pdo->query($sqlSelectData);
 
-  $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+<h1>
+  Eduardo Osteicoechea
+</h1>
 
-  echo "<hr><h2>Users Table Content:</h2>";
 
-  if ($users) {
-    echo "<table border='1' style='width:100%; border-collapse: collapse;'>";
-    echo "<thead><tr><th>ID</th><th>Name</th><th>Email</th></tr></thead>";
-    echo "<tbody>";
+<h2>
+  This app was developed with Eduardo Osteicoechea\'s Minimal Framework
+</h2>
 
-    foreach ($users as $user) {
-      echo "<tr>";
-      echo "<td>" . htmlspecialchars($user['id']) . "</td>";
-      echo "<td>" . htmlspecialchars($user['name']) . "</td>";
-      echo "<td>" . htmlspecialchars($user['email']) . "</td>";
-      echo "</tr>";
-    }
+<p>
+  Core values:
 
-    echo "</tbody>";
-    echo "</table>";
-  } else {
-    echo "<p>No users found in the database.</p>";
-  }
-} catch (PDOException $e) {
-  echo "Error: " . $e->getMessage();
-}
+  - Cost effective (40$/month),
+  - Instant CI/CD, 
+  - Client Side Hidratable, 
+  - Server side Rendered, 
+  - Natively Coded,
+  - Full Stack Web Applications
+  - For Small Businesses
 
-$pdo = null;
+  Before disclosing it\'s architecture and components, let me explain the problem that I\'m solving, and then show you why this is the best option on the industry.
+
+  1. The best solution solves the biggest problem.
+
+  2. The secrets for developer productivity: Universality, Instant changes in production, toolchain minimalism.
+
+  3. Poverty and International restrictions, the fuel of the framework:
+
+  4. Failed attemps: Node.js, .NET minimal api\'s, Go, Rust.
+
+  5. The five star stack: SQLite, PHP, JavaScript, CSS, HTML
+
+  6. The language choice philosophy: If you have the capacity to work within the JavaScript Framework Madness, you\'re able to code in native languages for small businesses. When you have thousands of users, you hire specialized professionals to rewrite on whatever you want.
+</p>
+
+
+
+
+
+
+
+';
